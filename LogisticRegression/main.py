0c21ca2e96a864904c64af87b4b2d84fdbdc7c5f
SebastienC <91947798+SebastienCherki@users.noreply.github.com>
2023-11-29 08:45:34 +0100

    Update main.py

--- LogisticRegression/main.py
+++ LogisticRegression/main.py
@@ -1,17 +1,22 @@
 # Importation des modules nécessaires
-from sklearn.linear_model import LogisticRegression
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, binarize
-from sklearn.metrics import precision_score, recall_score, roc_auc_score, confusion_matrix, roc_curve, auc, classification_report
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
+from sklearn.linear_model import LogisticRegression  # Importation du modèle de régression logistique
+import pandas as pd  # Importation de la bibliothèque pandas pour la manipulation des données
+from sklearn.model_selection import train_test_split  # Importation de la fonction pour diviser le jeu de données
+from sklearn.preprocessing import LabelEncoder, binarize  # Importation des outils de prétraitement des données
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, confusion_matrix, roc_curve, auc, classification_report  # Importation des métriques d'évaluation
+import matplotlib.pyplot as plt  # Importation de la bibliothèque pour la visualisation
+import seaborn as sns  # Importation de la bibliothèque de visualisation avancée
+import os  # Importation du module pour travailler avec les chemins de fichiers
 
-# Fonction pour plotter la matrice de confusion
+# Fonction pour afficher la matrice de confusion
 def plot_confusion_matrix(y_true, y_scores, threshold=0.5):
+    # Binarisation des prédictions avec un seuil spécifié
     y_pred = (y_scores > threshold).astype(int)
+    
+    # Calcul de la matrice de confusion
     cm = confusion_matrix(y_true, y_pred)
+    
+    # Plot de la matrice de confusion avec seaborn
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False,
                 xticklabels=["Pas de Fraude", "Fraude"],
@@ -22,12 +27,12 @@ def plot_confusion_matrix(y_true, y_scores, threshold=0.5):
     plt.show()
 
 # Chargement du fichier CSV nettoyé depuis le chemin spécifique
-script_directory = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_directory, '..', 'data', 'bank.csv')
-df = pd.read_csv(file_path)
+script_directory = os.path.dirname(os.path.abspath(__file__))  # Obtention du répertoire du script
+file_path = os.path.join(script_directory, '..', 'data', 'bank.csv')  # Construction du chemin du fichier CSV
+df = pd.read_csv(file_path)  # Chargement du fichier CSV dans un DataFrame
 
 # Initialisation de la fonction LabelEncoder()
-label_encoder = LabelEncoder()
+label_encoder = LabelEncoder()  # Initialisation de l'encodeur de libellé pour convertir les catégories en nombres
 
 # Conversion des données catégorielles en données numériques utilisables par le modèle
 df['type'] = label_encoder.fit_transform(df['type'])
@@ -35,29 +40,29 @@ df['nameOrig'] = label_encoder.fit_transform(df['nameOrig'])
 df['nameDest'] = label_encoder.fit_transform(df['nameDest'])
 
 # Définition de la donnée cible
-X = df.drop('isFraud', axis=1)
-y = df['isFraud']
+X = df.drop('isFraud', axis=1)  # Les caractéristiques du modèle
+y = df['isFraud']  # La variable cible à prédire
 
 # Binarisation de la variable cible
-y_binary = binarize(y.values.reshape(-1, 1)).ravel()
+y_binary = binarize(y.values.reshape(-1, 1)).ravel()  # Binarisation des valeurs pour les classes (0 ou 1)
 
 # Division du Dataset en un ensemble d'entraînement et un ensemble de test
-X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.2, random_state=125)
+X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.2, random_state=125)  # Division du jeu de données
 
 # Logistic Regression
-logreg_classifier = LogisticRegression(random_state=125)
-logreg_classifier.fit(X_train, y_train)
+logreg_classifier = LogisticRegression(random_state=125)  # Initialisation du modèle de régression logistique
+logreg_classifier.fit(X_train, y_train)  # Entraînement du modèle sur l'ensemble d'entraînement
 
 # Test du modèle
-predictions = logreg_classifier.predict(X_test)
+predictions = logreg_classifier.predict(X_test)  # Prédictions sur l'ensemble de test
 
 # Binarisation des prédictions
-predictions_binary = binarize(predictions.reshape(-1, 1)).ravel()
+predictions_binary = binarize(predictions.reshape(-1, 1)).ravel()  # Binarisation des valeurs prédites
 
 # Calcul de la précision, du rappel et de l'AUC
-precision = precision_score(y_test, predictions_binary)
-recall = recall_score(y_test, predictions_binary)
-roc_auc = roc_auc_score(y_test, predictions)
+precision = precision_score(y_test, predictions_binary)  # Calcul de la précision
+recall = recall_score(y_test, predictions_binary)  # Calcul du rappel
+roc_auc = roc_auc_score(y_test, predictions)  # Calcul de l'AUC
 
 # Affichage des résultats d'évaluation
 print("Précision: {:.2f}".format(precision))
@@ -65,7 +70,7 @@ print("Rappel: {:.2f}".format(recall))
 print("Aire sous la courbe ROC (AUC): {:.2f}".format(roc_auc))
 
 # Évaluer les performances du modèle avec confusion_matrix
-cm = confusion_matrix(y_test, predictions_binary)
+cm = confusion_matrix(y_test, predictions_binary)  # Calcul de la matrice de confusion
 print("Matrice de Confusion:")
 print(cm)
 
@@ -73,11 +78,11 @@ print(cm)
 plot_confusion_matrix(y_test, predictions_binary)
 
 # Afficher la courbe ROC
-fpr, tpr, thresholds = roc_curve(y_test, predictions)
-area_under_curve = auc(fpr, tpr)
+fpr, tpr, thresholds = roc_curve(y_test, predictions)  # Calcul des valeurs pour la courbe ROC
+area_under_curve = auc(fpr, tpr)  # Calcul de l'aire sous la courbe ROC
 
 plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='Courbe ROC (AUC = {:.2f})'.format(area_under_curve))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='Courbe ROC (AUC = {:.2f})'.format(area_under_curve))  # Plot de la courbe ROC
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlabel('Taux de faux positifs')
 plt.ylabel('Taux de vrais positifs')
